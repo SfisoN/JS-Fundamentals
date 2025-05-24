2fca5e64b3739ebfbd15bbdb1068e68d9b226bec
Sifiso <sifisontulisn88@gmail.com>
2025-05-24 14:29:49 +0200

    Correction of code

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,13 @@
 const x = parseInt(process.argv[2]);
 
-if(isNaN(x)) {
-  console.log("Missing number of occurences.");
-}else{
-    let i = 0;
-    while(i < x) {
-        console.log("C is fun");
-        i++;
+for(let j=0; j < x; j++) {
+    if(isNaN(x)){
+        console.log("Missing number of occurrences");
+    }else{
+        let i = 0;
+        while (i < x) {
+            console.log("C is fun");
+            i++;
+        }
     }
 }
