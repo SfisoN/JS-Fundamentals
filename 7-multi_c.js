efe463d8eb00b0c07762c46987b092c3a17c7409
Sifiso <sifisontulisn88@gmail.com>
2025-05-24 14:32:08 +0200

    Correction

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -5,7 +5,7 @@ for(let j=0; j < x; j++) {
         console.log("Missing number of occurrences");
     }else{
         let i = 0;
-        while (i < x) {
+        while (i < 6) {
             console.log("C is fun");
             i++;
         }
