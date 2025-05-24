545e3247b50354823d76490dc154276d5c95b045
Sifiso <sifisontulisn88@gmail.com>
2025-05-24 14:48:08 +0200

    New add JS file

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+
+function add(a, b) {
+  return a + b;
+}
+
+
+const firstNum = parseInt(process.argv[2], 10);
+const secondNum = parseInt(process.argv[3], 10);
+
+
+console.log(add(firstNum, secondNum));
